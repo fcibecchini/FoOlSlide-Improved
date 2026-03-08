6e036b2c1df65d14d2a0a23c27e4b66788ea7c0d
Fabio Cibecchini <6901081+fcibecchini@users.noreply.github.com>
2026-03-08 20:05:03 +0100

    fix: keep smooth mobile reader navigation when not zoomed (#25)

--- content/themes/default/views/layouts/reader.php
+++ content/themes/default/views/layouts/reader.php
@@ -4,7 +4,7 @@
 <head>
 <title><?php echo $template['title']; ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10, user-scalable=yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php
 		if (isset($metacomic))
 		{
